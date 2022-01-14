7d631e6c30911570c64f5b55890c1887080a3ece
den45235 <96654197+den45235@users.noreply.github.com>
2022-01-14 09:53:42 +0300

    1

---
+++ venv/test.py
@@ -0,0 +1,14 @@
+from imageai.Detection import ObjectDetection
+import os
+
+exec_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(exec_path, "resnet50_coco_best_v2.1.0.h5")
+detector.loadModel()
+
+list = detector.detectObjectsFromImage(
+    input_image=os.path.join(exec_path, "objects.jpg"),
+    output_image_path=os.path.join(exec_path, "new_objects.jpg"
+)
