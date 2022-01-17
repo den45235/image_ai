7afe0287fcc5c6e485a0eceef3e88b57744f7eeb
den45235 <96654197+den45235@users.noreply.github.com>
2022-01-17 13:39:27 +0300

    5

--- venv/test.py
+++ venv/test.py
@@ -11,7 +11,7 @@ detector.loadModel()
 list = detector.detectObjectsFromImage(
     input_image=os.path.join(exec_path, "objects.jpg"),
     output_image_path=os.path.join(exec_path, "new_objects.jpg"),
-    #minimum_percentage_probability=70,
+    # minimum_percentage_probability=70,
     #display_percentage_probability=False,
     #display_object_name=True
 )
