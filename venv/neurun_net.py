ef87208d98b604c56a7ef57d77ae0040ffc8c627
den45235 <96654197+den45235@users.noreply.github.com>
2022-01-17 16:50:02 +0300

    6

---
+++ venv/neurun_net.py
@@ -0,0 +1,28 @@
+import os
+os.environ ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras.layers import Dense
+
+c = np.array([-40, -10, 0, 8, 15, 22, 38])
+f = np.array([-40, 14, 32, 46, 59, 72, 100])
+
+model = keras.Sequential()
+model.add(Dense(units=1, input_shape=(1,), activation='linear'))
+
+model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(0.1))
+
+history = model.fit(c, f, epochs=500, verbose=0)
+
+print("обучение завершено")
+
+plt.plot(history.history['loss'])
+plt.grid(True)
+plt.show()
+
+print(model.predict([435235]))
+print(model.get_weights())
+
+
